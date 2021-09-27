49d81cca842aa904fa2bed77d61e5285b391bc64
Irvin Torres <itorres@devtch.com>
2021-09-26 19:48:13 -0600

    Dummy data

---
+++ design/DeliveryDummyData.sql
@@ -0,0 +1,15 @@
+use delivery;
+insert into roles values (null,"ROLE_Admin"),(null,"ROLE_Customer");
+
+insert into product_type values (null,'Beverages'),(null,'main course');
+insert into product values 
+(null,'Soda',1.00,1),
+(null,'Ice Tea',1.00,1),
+(null,'Beer',1.50,1),
+(null,'Water',0.90,1),
+(null,'Beef',0003.00,2),
+(null,'Meat',0002.00,2),
+(null,'Chiken',0002.00,2),
+(null,'Hot Dog',0001.00,2),
+(null,'Hamburger',0002.00,2),
+(null,'Pizza',0005.00,2);
